4c657c10c4af360a1a5ef3193c8a506526ba45eb
Ilya Liverts <ilya@boredomtherapy.com>
2019-03-16 17:31:56 +0200

    16

--- backend/src/server/flask_server.py
+++ backend/src/server/flask_server.py
@@ -48,9 +48,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/gethostname")
 def hello():
-    return "Hello World from Flask"
+    return flask.request.host
 
 if __name__ == "__main__":
     # Only for debugging while developing
