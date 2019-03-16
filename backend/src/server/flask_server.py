0700743fc845d6a62b96f16159aa38acdc6316bf
Ilya Liverts <ilya@boredomtherapy.com>
2019-03-16 15:46:29 +0200

    13

--- backend/src/server/flask_server.py
+++ backend/src/server/flask_server.py
@@ -41,6 +41,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         w.stop(timeout=10)
 
-@app.route('/')
-def hello_world():
-    return request.host_url
+@app.route('/hello')
+def hello():
+    return "Hello World!"
